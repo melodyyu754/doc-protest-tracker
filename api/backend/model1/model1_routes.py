810ce75b953a3fd6d859d23bce70df476f4e2a0a
Melody Yu <yu.melo@northeastern.edu>
2024-06-06 09:17:11 +0200

    uncommented something

--- api/backend/model1/model1_routes.py
+++ api/backend/model1/model1_routes.py
@@ -1,20 +1,20 @@
-# ########################################################
-# # Sample customers blueprint of endpoints
-# # Remove this file if you are not using it in your project
-# ########################################################
-# from flask import Blueprint, request, jsonify, make_response, current_app
+########################################################
+# Sample customers blueprint of endpoints
+# Remove this file if you are not using it in your project
+########################################################
+from flask import Blueprint, request, jsonify, make_response, current_app
 # import json
-# from backend.db_connection import db
-# from backend.model1.model1_functions import predict
+from backend.db_connection import db
+from backend.model1.model1_functions import predict
 
-# model1 = Blueprint('model1', __name__)
+model1 = Blueprint('model1', __name__)
 
-# # Get all customers from the DB
-# @model1.route('/model1/<var01>/<var02>/<var03>', methods=['GET'])
-# def predicting_amount_of_protests(var01, var02, var03):
-#     current_app.logger.info(f'var01 = {var01}')
-#     current_app.logger.info(f'var02 = {var02}')
-#     current_app.logger.info(f'var03 = {var03}')
+# Get all customers from the DB
+@model1.route('/model1/<var01>/<var02>/<var03>', methods=['GET'])
+def predicting_amount_of_protests(var01, var02, var03):
+    current_app.logger.info(f'var01 = {var01}')
+    current_app.logger.info(f'var02 = {var02}')
+    current_app.logger.info(f'var03 = {var03}')
 
     prediction = predict(var01, var02, var03)
     #print(prediction)
