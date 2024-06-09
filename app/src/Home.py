884c2ef3f25e53ea790545c8de61c6e0a4661467
Sonia Sheth <sheth.son@northeastern.edu>
2024-06-09 23:33:00 +0200

    automatically load the username in for new posts and protests

--- app/src/Home.py
+++ app/src/Home.py
@@ -21,6 +21,7 @@ if st.button("Act as Sally, a student activist at Columbia University",
     st.session_state['authenticated'] = True
     st.session_state['role'] = 'activist'
     st.session_state['first_name'] = 'Sally'
+    st.session_state['last_name'] = 'Clark'
     st.session_state['user_id'] = 1
     st.session_state['post_id'] = 0
     st.switch_page('pages/00_Activist_Home.py')
@@ -30,7 +31,8 @@ if st.button('Act as McGuinness, the president of the United States',
             use_container_width=True):
     st.session_state['authenticated'] = True
     st.session_state['role'] = 'politician'
-    st.session_state['first_name'] = 'McGuinness'
+    st.session_state['first_name'] = 'Peter'
+    st.session_state['last_name'] = 'McGuinness'
     st.session_state['user_id'] = 2
     st.switch_page('pages/01_Politician_Home.py')
 
@@ -40,5 +42,6 @@ if st.button('Act as Sydney, a reporter at the New York Times',
     st.session_state['authenticated'] = True
     st.session_state['role'] = 'journalist'
     st.session_state['first_name'] = 'Sydney'
+    st.session_state['last_name'] = 'Stone'
     st.session_state['user_id'] = 3
     st.switch_page('pages/02_Journalist_Home.py')
