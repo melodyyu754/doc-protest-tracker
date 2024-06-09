d8215a6206a6c0487e8fccbafa23284387bf7d77
Melody Yu <yu.melo@northeastern.edu>
2024-06-09 10:15:51 +0200

    model 2 takes in database

--- app/src/modules/nav.py
+++ app/src/modules/nav.py
@@ -136,6 +136,7 @@ def SideBarLinks(show_home=False):
             ViewCountriesNav()
             CompareCountriesNav()
             ViewModel1Nav()
+            ViewModel2Nav()
 
     # Always show the About page at the bottom of the list of links
     AboutPageNav()
